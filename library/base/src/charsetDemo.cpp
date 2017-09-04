a2bd237638b19347439fc83e45fad66cd1390d6f
lzw978 <lzw978@163.com>
2017-09-04 18:04:56 +0800

    优化案例程序

--- library/base/src/charsetDemo.cpp
+++ library/base/src/charsetDemo.cpp
@@ -14,6 +14,8 @@
  *   Organization:
  *   编译src目录下执行：
  * g++ -g -o charsetDemo -I../include -L../ -lCommBase -lrt charsetDemo.cpp
+ * 先把文件编码设置为GBK, 验证GBK转换UTF8, 注释34行
+ * 再把文件编码设置为UTF8，验证UTF8转换为GBK, 注释35行
  * =====================================================================================
  */
 
@@ -27,8 +29,9 @@ using namespace commbase;
 using namespace std;
 int main(int argc, char** argv)
 {
-    string utf8Msg = "test中文UTF8编码转换为GBK信息,验证是否可以正常显示";
-    cout << "before = [" << utf8Msg  << "]" << endl;
-    cout << "after  = [" << _U2G(utf8Msg) << "]" << endl;
+    string codeMsg = "中文编码转换为信息,验证是否可以正常显示";
+    cout << "before = [" << codeMsg  << "]" << endl;
+    cout << "after  = [" << _U2G(codeMsg) << "]" << endl;
+    //cout << "after  = [" << _G2U(codeMsg) << "]" << endl;
     return -1;
 }
